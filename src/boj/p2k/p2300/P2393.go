3d2971564c64ffcdb56000f1bc1ce0c034863a35
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2024-07-24 22:30:33 +0900

    solve: 2393 Rook

---
+++ src/boj/p2k/p2300/P2393.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println("  ___  ___  ___")
+    fmt.Println("  | |__| |__| |")
+    fmt.Println("  |           |")
+    fmt.Println("   \\_________/")
+    fmt.Println("    \\_______/")
+    fmt.Println("     |     |")
+    fmt.Println("     |     |")
+    fmt.Println("     |     |")
+    fmt.Println("     |     |")
+    fmt.Println("     |_____|")
+    fmt.Println("  __/       \\__")
+    fmt.Println(" /             \\")
+    fmt.Println("/_______________\\")
+}
